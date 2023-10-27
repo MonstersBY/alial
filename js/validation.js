1ca0b27c6970e36f049a6e2d8b3a1777c76ded8d
Gasay-Liza <gasayliz@yandex.ru>
2023-10-27 13:56:30 +0300

    feat: validation on main page

--- js/validation.js
+++ js/validation.js
@@ -53,3 +53,58 @@ $(document).ready(function () {
         }
     });
 });
+
+// На главной
+$(document).ready(function () {
+    $('.feedback-form').on('submit', function (event) {
+        event.preventDefault();
+    });
+
+    $('.feedback-form').validate({
+        highlight: function (element, errorClass) {
+            $(element).add($(element).parent()).addClass("invalid");
+        },
+        unhighlight: function (element, errorClass) {
+            $(element).add($(element).parent()).removeClass("invalid");
+        },
+        errorElement: "span",
+        errorClass: "error",
+        rules: {
+            name: {
+                required: true,
+            },
+            telephone: {
+                required: true,
+            },
+            email: {
+                required: true,
+                email: true,
+            },
+            company: {
+                required: true,
+            },
+            message: {
+                required: true,
+            },
+        },
+
+        messages: {
+            name: {
+                required: "Это поле обязательно для заполнения",
+            },
+            telephone: {
+                required: "Это поле обязательно для заполнения",
+            },
+            email: {
+                required: "Это поле обязательно для заполнения",
+                email: "Пожалуйста, введите корректный адрес электронной почты",
+            },
+            company: {
+                required: "Это поле обязательно для заполнения",
+            },
+            message: {
+                required: "Это поле обязательно для заполнения",
+            },
+        }
+    });
+});
